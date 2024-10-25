388a0590accdeb20f2ffae1ccfbcd236ba68b567
Jorge <j.tenoriogarcia05@gmail.com>
2024-10-25 10:06:29 +0200

    decodificar ya funciona

--- src/test/scala/ArbolHuffman.scala
+++ src/test/scala/ArbolHuffman.scala
@@ -1,3 +1,5 @@
+import scala.annotation.tailrec
+
 type Bit = 0 | 1
 
 def cadenaAListaChars(cadena:String):List[Char]=
@@ -7,7 +9,7 @@ def listaCharsACadena(caracteres:List[Char]):String=
   caracteres.mkString
 
 
-abstract class ArbolHuffman{
+abstract class ArbolHuffman {
   def peso(arbol: ArbolHuffman): Int = arbol match
     case HojaHuff(_, peso) => peso
     case RamaHuff(nodoIzq, nodoDcho) => peso(nodoIzq) + peso(nodoDcho)
@@ -15,12 +17,23 @@ abstract class ArbolHuffman{
   def caracteres(arbol: ArbolHuffman): List[Char] = arbol match
     case HojaHuff(caracter, _) => List(caracter)
     case RamaHuff(nodoIzq, nodoDcho) => caracteres(nodoIzq) ++ caracteres(nodoDcho)
-  def codificar(bits:List[Bit]):String=
 
+  def decodificar(bits: List[Bit]): String =
+    @annotation.tailrec
+    def decodificarAux(actual: ArbolHuffman, bitsRestantes: List[Bit], codigo: List[Char]): List[Char] = (actual, bitsRestantes) match
+      case (_, Nil) => codigo
+
+      case (hojaHuff: HojaHuff, _) => decodificarAux(this, bitsRestantes, codigo :+ hojaHuff.caracter)
+
+      case (ramaHuff: RamaHuff, 0 :: bitsRestantes) => decodificarAux(ramaHuff.nodoIzq, bitsRestantes, codigo)
+
+      case (ramaHuff: RamaHuff, 1 :: bitsRestantes) => decodificarAux(ramaHuff.nodoDch, bitsRestantes, codigo)
+
+    listaCharsACadena(decodificarAux(this, bits, List.empty[Char]))
 }
 
 
-case class RamaHuff(nodoIzq: ArbolHuffman, nodoDch: ArbolHuffman) extends ArbolHuffman
+  case class RamaHuff(nodoIzq: ArbolHuffman, nodoDch: ArbolHuffman) extends ArbolHuffman
       
-case class HojaHuff(caracter: Char, peso: Int) extends ArbolHuffman
+  case class HojaHuff(caracter: Char, peso: Int) extends ArbolHuffman
 
--- src/test/scala/Main.scala
+++ src/test/scala/Main.scala
@@ -6,13 +6,14 @@ object Main extends ArbolHuffman {
     val hojaO = HojaHuff('O', 3)
     val hojaE = HojaHuff('E', 2)
     val hojaEspacio = HojaHuff(' ', 2)
-
     val rama1 = RamaHuff(hojaE, hojaEspacio)
     val rama2 = RamaHuff(rama1, hojaO)
     val arbolHuffman = RamaHuff(hojaS, rama2)
+    val listabits:List[Bit]=List(0,1,0,0,1,1,1,1,1,0,0,1,1,0,1,1,1,1,0,0,1,0)
 
     print(s"Peso total del árbol: ${peso  (arbolHuffman)}\n")
     print(s"Caracteres del árbol: ${caracteres(arbolHuffman)}\n")
+    print(s"cadena 0100101101:${arbolHuffman.decodificar(listabits)}\n")
 
   }
 
